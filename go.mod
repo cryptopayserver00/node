117a81ab27a25da2cf0313ebd87f5bdfbbe0c327
viper-00 <zhongmingyang2000@gmail.com>
2025-02-09 22:18:28 +0800

    Remove: useless pkg for tron chain

--- go.mod
+++ go.mod
@@ -8,11 +8,9 @@ require (
 	github.com/blocto/solana-go-sdk v1.30.0
 	github.com/bradfitz/gomemcache v0.0.0-20230905024940-24af94b03874
 	github.com/btcsuite/btcd v0.24.2
-	github.com/btcsuite/btcd/btcec/v2 v2.3.2
 	github.com/btcsuite/btcd/btcutil v1.1.6
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0
 	github.com/ethereum/go-ethereum v1.14.13
-	github.com/fbsobreira/gotron-sdk v0.0.0-20230907131216-1e824406fe8c
 	github.com/fsnotify/fsnotify v1.8.0
 	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.9.1
@@ -25,8 +23,6 @@ require (
 	github.com/xrpscan/xrpl-go v0.2.7
 	github.com/xssnick/tonutils-go v1.10.2
 	go.uber.org/zap v1.27.0
-	google.golang.org/grpc v1.67.3
-	google.golang.org/protobuf v1.36.1
 	gopkg.in/telebot.v3 v3.2.1
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
@@ -37,6 +33,7 @@ require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/bits-and-blooms/bitset v1.20.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/btcsuite/btclog v1.0.0 // indirect
 	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
@@ -46,7 +43,6 @@ require (
 	github.com/consensys/gnark-crypto v0.16.0 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.1.0 // indirect
-	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
@@ -60,7 +56,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -73,20 +68,15 @@ require (
 	github.com/magiconair/properties v1.8.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/oasisprotocol/curve25519-voi v0.0.0-20220328075252-7dd334e3daae // indirect
-	github.com/pborman/uuid v1.2.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rjeczalik/notify v0.9.3 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/shengdoushi/base58 v1.0.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/sigurn/crc16 v0.0.0-20211026045750-20ab5afb07e3 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -98,7 +88,6 @@ require (
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.6.0 // indirect
@@ -108,8 +97,7 @@ require (
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20241209162323-e6fa225c2576 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241223144023-3abc09e42ca8 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
--- utils/wallet/tron.go
+++ utils/wallet/tron.go
@@ -1,137 +1,128 @@
 package wallet
 
-import (
-	"crypto/sha256"
-	"encoding/hex"
-	"errors"
-	"node/global"
-	"node/global/constant"
-	sweepUtils "node/sweep/utils"
-	"node/utils"
-
-	"github.com/btcsuite/btcd/btcec/v2"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/fbsobreira/gotron-sdk/pkg/client"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/protobuf/proto"
-)
-
 func SendTrxTransfer(chainId uint, pri, pub, toAddress string, sendVal string) (hash string, err error) {
-
-	url := constant.GetHttpUrlByNetwork(chainId)
-	if url == "" {
-		err = errors.New("chain not support")
-		return
-	}
-
-	c := client.NewGrpcClient(url)
-	err = c.Start(grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return
-	}
-
-	sendValue, err := utils.FormatToOriginalValue(sendVal, 6)
-	if err != nil {
-		return "", err
-	}
-
-	tx, err := c.Transfer(pub, toAddress, sendValue.Int64())
-	if err != nil {
-		return
-	}
-
-	rawData, err := proto.Marshal(tx.Transaction.GetRawData())
-	if err != nil {
-		return
-	}
-
-	h256h := sha256.New()
-	h256h.Write(rawData)
-	txHash := h256h.Sum(nil)
-
-	privateKeyBytes, err := hex.DecodeString(pri)
-	if err != nil {
-		return
-	}
-
-	sk, _ := btcec.PrivKeyFromBytes(privateKeyBytes)
-
-	signature, err := crypto.Sign(txHash, sk.ToECDSA())
-	if err != nil {
-		return
-	}
-
-	tx.Transaction.Signature = append(tx.Transaction.Signature, signature)
-
-	result, err := c.Broadcast(tx.Transaction)
-	if err != nil {
-		return
-	}
-
-	global.NODE_LOG.Info("SendTrxTransfer: " + string(result.Message))
-
 	return "", nil
 }
 
 func SendTronTokenTransfer(chainId uint, pri, pub, toAddress, coin string, sendVal string) (hash string, err error) {
-
-	url := constant.GetHttpUrlByNetwork(chainId)
-	if url == "" {
-		err = errors.New("chain not support")
-		return
-	}
-
-	c := client.NewGrpcClient(url)
-	err = c.Start(grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return
-	}
-
-	isSupport, _, _, decimals := sweepUtils.GetContractInfoByChainIdAndSymbol(chainId, coin)
-	if !isSupport {
-		return "", errors.New("contract address not found")
-	}
-
-	sendValue, err := utils.FormatToOriginalValue(sendVal, decimals)
-	if err != nil {
-		return "", err
-	}
-
-	tx, err := c.TransferAsset(pub, toAddress, coin, sendValue.Int64())
-	if err != nil {
-		return
-	}
-
-	rawData, err := proto.Marshal(tx.Transaction.GetRawData())
-	if err != nil {
-		return
-	}
-
-	h256h := sha256.New()
-	h256h.Write(rawData)
-	txHash := h256h.Sum(nil)
-
-	privateKeyBytes, err := hex.DecodeString(pri)
-	if err != nil {
-		return
-	}
-
-	sk, _ := btcec.PrivKeyFromBytes(privateKeyBytes)
-
-	signature, err := crypto.Sign(txHash, sk.ToECDSA())
-	if err != nil {
-		return
-	}
-
-	tx.Transaction.Signature = append(tx.Transaction.Signature, signature)
-
-	result, err := c.Broadcast(tx.Transaction)
-	if err != nil {
-		return
-	}
-
-	global.NODE_LOG.Info("SendTronTokenTransfer: " + string(result.Message))
-
 	return "", nil
 }
+
+// func SendTrxTransfer(chainId uint, pri, pub, toAddress string, sendVal string) (hash string, err error) {
+
+// 	url := constant.GetHttpUrlByNetwork(chainId)
+// 	if url == "" {
+// 		err = errors.New("chain not support")
+// 		return
+// 	}
+
+// 	c := client.NewGrpcClient(url)
+// 	err = c.Start(grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	sendValue, err := utils.FormatToOriginalValue(sendVal, 6)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	tx, err := c.Transfer(pub, toAddress, sendValue.Int64())
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	rawData, err := proto.Marshal(tx.Transaction.GetRawData())
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	h256h := sha256.New()
+// 	h256h.Write(rawData)
+// 	txHash := h256h.Sum(nil)
+
+// 	privateKeyBytes, err := hex.DecodeString(pri)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	sk, _ := btcec.PrivKeyFromBytes(privateKeyBytes)
+
+// 	signature, err := crypto.Sign(txHash, sk.ToECDSA())
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	tx.Transaction.Signature = append(tx.Transaction.Signature, signature)
+
+// 	result, err := c.Broadcast(tx.Transaction)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	global.NODE_LOG.Info("SendTrxTransfer: " + string(result.Message))
+
+// 	return "", nil
+// }
+
+// func SendTronTokenTransfer(chainId uint, pri, pub, toAddress, coin string, sendVal string) (hash string, err error) {
+
+// 	url := constant.GetHttpUrlByNetwork(chainId)
+// 	if url == "" {
+// 		err = errors.New("chain not support")
+// 		return
+// 	}
+
+// 	c := client.NewGrpcClient(url)
+// 	err = c.Start(grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	isSupport, _, _, decimals := sweepUtils.GetContractInfoByChainIdAndSymbol(chainId, coin)
+// 	if !isSupport {
+// 		return "", errors.New("contract address not found")
+// 	}
+
+// 	sendValue, err := utils.FormatToOriginalValue(sendVal, decimals)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	tx, err := c.TransferAsset(pub, toAddress, coin, sendValue.Int64())
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	rawData, err := proto.Marshal(tx.Transaction.GetRawData())
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	h256h := sha256.New()
+// 	h256h.Write(rawData)
+// 	txHash := h256h.Sum(nil)
+
+// 	privateKeyBytes, err := hex.DecodeString(pri)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	sk, _ := btcec.PrivKeyFromBytes(privateKeyBytes)
+
+// 	signature, err := crypto.Sign(txHash, sk.ToECDSA())
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	tx.Transaction.Signature = append(tx.Transaction.Signature, signature)
+
+// 	result, err := c.Broadcast(tx.Transaction)
+// 	if err != nil {
+// 		return
+// 	}
+
+// 	global.NODE_LOG.Info("SendTronTokenTransfer: " + string(result.Message))
+
+// 	return "", nil
+// }
